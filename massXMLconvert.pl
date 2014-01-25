7886e86dafc56bf31d2a01b1bdeab9ca342ea310
Mike Coutermarsh <coutermarsh.mike@gmail.com>
2014-01-25 13:05:49 -0500

    Update massXMLconvert.pl

--- massXMLconvert.pl
+++ massXMLconvert.pl
@@ -2,11 +2,11 @@
 # Uses convert.pl to convert each XML file in the current directory to .csv
 @files = <*>;
  foreach $file (@files) {
- 	if($file =~ m/\.xml/){
- 		@filename = split('\.', $file);
- 		$name = $filename[0];
- 		print "converting $file...\n";
-   		`perl convert.pl $file >$name.csv`;
-	}
+  if($file =~ m/\.xml/) {
+    @filename = split('\.', $file);
+    $name = $filename[0];
+    print "converting $file...\n";
+      `perl convert.pl $file >$name.csv`;
+  }
 }
-print "\ndone";
+print "\ndone :)";
