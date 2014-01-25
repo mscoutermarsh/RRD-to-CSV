61acba526731c31268f8ee1c6b31fe7b42768c52
Mike Coutermarsh <coutermarsh.mike@gmail.com>
2014-01-25 13:04:26 -0500

    fix tabs

--- convert.pl
+++ convert.pl
@@ -9,22 +9,22 @@ $inAverage = 0;
 # open xml file... read Line by Line
 open(MYINPUTFILE, $inputFile);
 while(<MYINPUTFILE>){
-	my($line) = $_;
-	chomp($line);
-	if($line =~ m/AVERAGE/){
-		$inAverage = 1;
-	}elsif($line =~ m/MAX/){
-		$inAverage = 0;
-	}
-	# this is ugly... But grabs the average for each day.
-	if($inAverage){
-		if($line =~ m/(00:00:00 UTC)/){ # You may need to change this to your timezone
-			if($line =~ m/(\d{4}-\d{2}-\d{2})/){
-				$date = $1;
-				if($line =~ m/(\d.\d*e\+\d+)<\/v><v>(\d.\d*e\+\d+)/){
-		  			print "$date,$1,$2\n";
-		  		}
-	  		}
-	  	}
+  my($line) = $_;
+  chomp($line);
+  if($line =~ m/AVERAGE/){
+    $inAverage = 1;
+  }elsif($line =~ m/MAX/){
+    $inAverage = 0;
+  }
+  # this is ugly... But grabs the average for each day.
+  if($inAverage){
+    if($line =~ m/(00:00:00 UTC)/){ # You may need to change this to your timezone
+      if($line =~ m/(\d{4}-\d{2}-\d{2})/){
+        $date = $1;
+        if($line =~ m/(\d.\d*e\+\d+)<\/v><v>(\d.\d*e\+\d+)/){
+            print "$date,$1,$2\n";
+          }
+        }
+      }
   }
  }
