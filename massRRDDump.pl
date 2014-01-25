0eda4488e0b4de907bf96a1285efa2cb657890d5
Mike Coutermarsh <coutermarsh.mike@gmail.com>
2014-01-25 13:05:15 -0500

    fix tabs

--- massRRDDump.pl
+++ massRRDDump.pl
@@ -3,11 +3,11 @@
 
 @files = <*>;
  foreach $file (@files) {
- 	if($file =~ m/\.rrd/){
- 		@filename = split('\.', $file);
- 		$name = $filename[0];
- 		print "dumping $file...\n";
-   		`rrdtool dump $file >$name.xml`;
-	}
+  if ($file =~ m/\.rrd/) {
+    @filename = split('\.', $file);
+    $name = $filename[0];
+    print "dumping $file...\n";
+      `rrdtool dump $file >$name.xml`;
+  }
 }
-print "done";
+print "done :)";
